095f00447473925ac54bfc05f5b2b8348733a100
Mohamed Ahmed <axmedmohamed7@gmail.com>
2021-10-09 11:33:41 -0500

    did the post header form

--- public/js/headerroutes.js
+++ public/js/headerroutes.js
@@ -1,28 +1,42 @@
-const userFirstNameInput = document.getElementById('userFirst');
-const userLastNameInput = document.getElementById('userLast');
-const userEmailInput = document.getElementById('userEmail');
-const userPhoneInput = document.getElementById('userPhone');
-const userAddressInput = document.getElementById('userAddress');
-const userCityInput = document.getElementById('userCity');
-const userStateInput = document.getElementById('userState');
-const userZipInput = document.getElementById('userZip');
-const postHeader = (review) =>
-    fetch('/api/header', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userFirstNameInput, userLastNameInput, userEmailInput, userPhoneInput, userAddressInput, userCityInput, userStateInput, userZipInput),
+const reviewForm = document.getElementById("review");
+
+reviewForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const userFirstNameInput = document.getElementById("userFirst").value;
+  const userLastNameInput = document.getElementById("userLast").value;
+  const userEmailInput = document.getElementById("userEmail").value;
+  const userPhoneInput = document.getElementById("userPhone").value;
+  const userAddressInput = document.getElementById("userAddress").value;
+  const userCityInput = document.getElementById("userCity").value;
+  const userStateInput = document.getElementById("userState").value;
+  const userZipInput = document.getElementById("userZip").value;
+  console.log("firstname", userFirstNameInput);
+  fetch("/api/header", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      userFirstNameInput,
+      userLastNameInput,
+      userEmailInput,
+      userPhoneInput,
+      userAddressInput,
+      userCityInput,
+      userStateInput,
+      userZipInput,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      console.log("Successful POST request:", data);
+      return data;
     })
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            console.log('Successful POST request:', data);
-            return data;
-        })
-        .catch((error) =>{
-            console.error('Error in POST request:', error);
-        })
-        reviewForm.addEventListener('submit', (e) => {
-            e.preventDefault();
-        })
+    .catch((error) => {
+      console.error("Error in POST request:", error);
+    });
+});
+
+
+
