501ba744044fcd00677b9d154c84d290fbe69d90
alynnwrd <83987321+alynnwrd@users.noreply.github.com>
2021-12-04 09:49:34 -0600

    making something for heroku

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const session = require('express-session');
 
-const sequelize = require("./config/connection");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+//const sequelize = require("./config/connection");
+//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+var compression = require('compression')
+  var compApp = express()
+    compApp.use(compression)
 
 
 const routes = require('./controllers');
@@ -16,17 +20,17 @@ const PORT = process.env.PORT || 3007;
 
 //const hbs = exphbs.create({ helpers });
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+//const sess = {
+  //secret: 'Super secret secret',
+  //cookie: {},
+  //resave: false,
+  //saveUninitialized: true,
+  //store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
 
-app.use(session(sess));
+//app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -41,8 +45,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening to the server"));
-});
+//sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening to the server on port, ", PORT));
+//});
 
-module.exports = sequelize;
+//module.exports = sequelize;
